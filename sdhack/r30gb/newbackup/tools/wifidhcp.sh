8c097d1e489f0376118ca3ae26c8eacbbc178b75
roleo <roleo@iperbole.bologna.it>
2020-11-25 16:53:47 +0100

    Fix hostname in dhcp client request

--- sdhack/r30gb/newbackup/tools/wifidhcp.sh
+++ sdhack/r30gb/newbackup/tools/wifidhcp.sh
@@ -1,3 +1,6 @@
 killall udhcpc
-udhcpc -i wlan0 -b -s /backup/tools/default.script -x hostname:$(hostname)
-
+HN="yi-hack"
+if [ -f /tmp/sd/yi-hack/etc/hostname ]; then
+        HN=$(cat /tmp/sd/yi-hack/etc/hostname)
+fi
+udhcpc -i wlan0 -b -s /backup/tools/default.script -x hostname:$HN

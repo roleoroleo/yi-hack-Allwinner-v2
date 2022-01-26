c4b866c49e72fdc31b040f0fecce04b68ed334e0
roleo <roleo@iperbole.bologna.it>
2022-01-26 12:36:52 +0100

    Remove init.sh from sdhack

--- sdhack/r35gb/Factory/config.sh
+++ sdhack/r35gb/Factory/config.sh
@@ -11,12 +11,25 @@ if [ `grep telnetd /backup/init.sh | grep -c ^` -gt 0 ]; then
 else
     echo "Applying hack"
 
-    ### Replace /backup/init.sh with a more friendly one
+    ### Update /backup/init.sh with a more friendly one
     echo "### Updating /backup/init.sh"
-    if [ -f /tmp/sd/newbackup/init.sh ]; then
-        cp -f /tmp/sd/newbackup/init.sh /backup/init.sh
-        chmod 0755 /backup/init.sh
-    fi
+    cp /backup/init.sh /tmp/init.sh
+    sed -n '1{$!N;$!N;$!N;$!N};$!N;s@\nif \[ \-f \/home\/app\/lower_half_init.sh \];then\n    source \/home\/app\/lower_half_init.sh\nelse\n    source \/backup\/lower_half_init.sh\nfi@@;P;D' -i /tmp/init.sh
+    sed -n '1{$!N;$!N;$!N;$!N};$!N;s@\nif \[ \-f \/home\/app\/lower_half_init.sh \];then\n\tsource \/home\/app\/lower_half_init.sh\nelse\n\tsource \/backup\/lower_half_init.sh\nfi@@;P;D' -i /tmp/init.sh
+
+    echo "# Running telnetd" >> /tmp/init.sh
+    echo "/usr/sbin/telnetd &" >> /tmp/init.sh
+    echo "" >> /tmp/init.sh
+    echo "if [ -f /tmp/sd/lower_half_init.sh ];then" >> /tmp/init.sh
+    echo "    source /tmp/sd/lower_half_init.sh" >> /tmp/init.sh
+    echo "elif [ -f /home/app/lower_half_init.sh ];then" >> /tmp/init.sh
+    echo "    source /home/app/lower_half_init.sh" >> /tmp/init.sh
+    echo "else" >> /tmp/init.sh
+    echo "    source /backup/lower_half_init.sh" >> /tmp/init.sh
+    echo "fi" >> /tmp/init.sh
+
+    cp /tmp/init.sh /backup/init.sh
+    rm /tmp/init.sh
 fi
 
 ### Disable the hack for next reboot

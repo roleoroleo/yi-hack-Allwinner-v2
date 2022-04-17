09170a49d2aed4022da4926ceb75d6ba69d587ad
roleo <roleo@iperbole.bologna.it>
2022-04-17 16:16:58 +0200

    Add backup to hacking procedure

--- sdhack/y29ga/Factory/config.sh
+++ sdhack/y29ga/Factory/config.sh
@@ -1,3 +1,23 @@
+echo "############## Starting Firmware Dump ##############"
+echo "############## Starting Firmware Dump ##############"
+echo "############## Starting Firmware Dump ##############"
+echo "############## Starting Firmware Dump ##############"
+
+mkdir -p /tmp/sd/backup
+
+cat /proc/mtd > /tmp/sd/backup/mtd.txt
+
+dd if=/dev/mtdblock0 of=/tmp/sd/backup/mtdblock0.bin
+dd if=/dev/mtdblock1 of=/tmp/sd/backup/mtdblock1.bin
+dd if=/dev/mtdblock2 of=/tmp/sd/backup/mtdblock2.bin
+dd if=/dev/mtdblock3 of=/tmp/sd/backup/mtdblock3.bin
+dd if=/dev/mtdblock4 of=/tmp/sd/backup/mtdblock4.bin
+dd if=/dev/mtdblock5 of=/tmp/sd/backup/mtdblock5.bin
+dd if=/dev/mtdblock6 of=/tmp/sd/backup/mtdblock6.bin
+dd if=/dev/mtdblock7 of=/tmp/sd/backup/mtdblock7.bin
+
+
+
 echo "############## Starting Hack ##############"
 echo "############## Starting Hack ##############"
 echo "############## Starting Hack ##############"

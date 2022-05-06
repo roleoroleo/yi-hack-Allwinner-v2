bf96b2413a9512e969afd5a43cc3113c7ddc48e1
roleo <roleo@iperbole.bologna.it>
2022-05-06 17:47:04 +0200

    Fix/improve rtsp watchdog

--- src/static/static/yi-hack/script/wd_rtsp.sh
+++ src/static/static/yi-hack/script/wd_rtsp.sh
@@ -48,9 +48,16 @@ restart_rtsp()
 check_rtsp()
 {
 #  echo "$(date +'%Y-%m-%d %H:%M:%S') - Checking RTSP process..." >> $LOG_FILE
-    SOCKET=`$YI_HACK_PREFIX/bin/netstat -an 2>&1 | grep ":$RTSP_PORT " | grep ESTABLISHED | grep -c ^`
+    LISTEN=`$YI_HACK_PREFIX/bin/netstat -an 2>&1 | grep ":$RTSP_PORT_NUMBER " | grep LISTEN | grep -c ^`
+    SOCKET=`$YI_HACK_PREFIX/bin/netstat -an 2>&1 | grep ":$RTSP_PORT_NUMBER " | grep ESTABLISHED | grep -c ^`
     CPU=`top -b -n 2 -d 1 | grep rRTSPServer | grep -v grep | tail -n 1 | awk '{print $8}'`
 
+    if [ $LISTEN -eq 0 ]; then
+        echo "$(date +'%Y-%m-%d %H:%M:%S') - Restarting rtsp process" >> $LOG_FILE
+        killall -q rRTSPServer
+        sleep 1
+        restart_rtsp
+    fi
     if [ "$CPU" == "" ]; then
         echo "$(date +'%Y-%m-%d %H:%M:%S') - No running processes, restarting..." >> $LOG_FILE
         killall -q rRTSPServer
@@ -78,9 +85,17 @@ check_rtsp()
 check_rtsp_alt()
 {
 #  echo "$(date +'%Y-%m-%d %H:%M:%S') - Checking RTSP process..." >> $LOG_FILE
+    LISTEN=`$YI_HACK_PREFIX/bin/netstat -an 2>&1 | grep ":$RTSP_PORT_NUMBER " | grep LISTEN | grep -c ^`
     CPU1=`top -b -n 2 -d 1 | grep h264grabber | grep -v grep | tail -n 1 | awk '{print $8}'`
     CPU2=`top -b -n 2 -d 1 | grep rtsp_server_yi | grep -v grep | tail -n 1 | awk '{print $8}'`
 
+    if [ $LISTEN -eq 0 ]; then
+        echo "$(date +'%Y-%m-%d %H:%M:%S') - Restarting rtsp process" >> $LOG_FILE
+        killall -q rtsp_server_yi
+        killall -q h264grabber
+        sleep 1
+        restart_rtsp
+    fi
     if [ "$CPU1" == "" ] || [ "$CPU2" == "" ]; then
         echo "$(date +'%Y-%m-%d %H:%M:%S') - No running processes, restarting..." >> $LOG_FILE
         killall -q rtsp_server_yi
@@ -129,6 +144,7 @@ if [ ! -z $RTSP_AUDIO_COMPRESSION ]; then
     RTSP_AUDIO_COMPRESSION="-a "$RTSP_AUDIO_COMPRESSION
 fi
 if [ ! -z $RTSP_PORT ]; then
+    RTSP_PORT_NUMBER=$RTSP_PORT
     RTSP_PORT="-p "$RTSP_PORT
 fi
 if [ ! -z $USERNAME ]; then

683e752c2a5e113fdca31dcd79037b60dac5da72
Filip Krakowski <krakowski@hhu.de>
2022-08-05 17:39:27 +0000

    Add library path during startup

--- src/static/static/yi-hack/script/system.sh
+++ src/static/static/yi-hack/script/system.sh
@@ -381,6 +381,9 @@ if [ -f "$YI_HACK_PREFIX/script/mqtt_advertise/startup.sh" ]; then
     $YI_HACK_PREFIX/script/mqtt_advertise/startup.sh
 fi
 
+# Add library path for linker
+echo "/tmp/sd/yi-hack/lib" > /etc/ld-musl-armhf.path
+
 # Remove log files written to SD on boot containing the WiFi password
 #rm -f "/tmp/sd/log/log_first_login.tar.gz"
 #rm -f "/tmp/sd/log/log_login.tar.gz"

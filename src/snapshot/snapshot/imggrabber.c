acb5bda9f270d8205bc11a1966cc4e23f9a49e9e
roleo <roleo@iperbole.bologna.it>
2022-08-25 11:36:20 +0200

    Add option to force low res snapshot

--- src/snapshot/snapshot/imggrabber.c
+++ src/snapshot/snapshot/imggrabber.c
@@ -631,6 +631,12 @@ int main(int argc, char **argv)
         return 0;
     }
 
+    // Check if snapshot is low res
+    if (access("/tmp/snapshot.low", F_OK ) == 0 ) {
+        fprintf(stderr, "Snapshot is low res\n");
+        res = RESOLUTION_LOW;
+    }
+
     // Check if the process is already running
     pid_t my_pid = getpid();
     if (proc_find(basename(argv[0]), my_pid) != -1) {
@@ -650,6 +656,7 @@ int main(int argc, char **argv)
             height = H_3K;
         }
     }
+    if (debug) fprintf(stderr, "Resolution %d x %d\n", width, height);
 
     if (file[0] == '\0') {
         // Read frames from frame buffer

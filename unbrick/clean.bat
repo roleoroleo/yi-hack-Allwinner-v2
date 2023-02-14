f7afe0d4e7a4e89aa8549b2958c647d7fa34b708
roleo <roleo@iperbole.bologna.it>
2023-02-14 15:55:52 +0100

    Improve unbrick scripts for Windows

--- unbrick/clean.bat
+++ unbrick/clean.bat
@@ -4,5 +4,7 @@ SETLOCAL EnableDelayedExpansion
 FOR /F %%x in ('dir /B /D /A:D *') do (
     SET DIR=%%x
     FOR /F "tokens=*" %%g IN ('type !DIR!\filename') do (SET FILENAME=%%g)
+    DEL !DIR!\rootfs_!FILENAME!.gz
+    DEL !DIR!\home_!FILENAME!.gz
     DEL !DIR!\backup_!FILENAME!.gz
 )
--- unbrick/clean.sh
+++ unbrick/clean.sh
@@ -3,6 +3,8 @@
 for DIR in * ; do
     if [ -d $DIR ]; then
         FILENAME=`cat $DIR/filename`
+        rm -f $DIR/rootfs_$FILENAME.gz
+        rm -f $DIR/home_$FILENAME.gz
         rm -rf $DIR/mnt
         rm -rf $DIR/backup
         rm -f $DIR/backup_$FILENAME.gz

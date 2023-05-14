0777436a27ca9c285b37a55d4292dac8c496ac4e
MaciekN <me@maciekn.pl>
2023-05-14 09:37:03 +0200

    Fix issue #623 Running process detection malfunctioning due to `ps` output being truncated

--- src/static/static/yi-hack/script/thumb.sh
+++ src/static/static/yi-hack/script/thumb.sh
@@ -157,7 +157,7 @@ serviceMain ()
 trap "" SIGHUP
 #
 if [ "${1}" = "cron" ]; then
-	RUNNING=$(ps | grep $SCRIPT_FULLFN | grep -v grep | grep /bin/sh | awk 'END { print NR }')
+	RUNNING=$(ps ww| grep $SCRIPT_FULLFN | grep -v grep | grep /bin/sh | awk 'END { print NR }')
 	if [ $RUNNING -gt 1 ]; then
 		logAdd "[INFO] === SERVICE ALREADY RUNNING ==="
 		exit 0
@@ -166,7 +166,7 @@ if [ "${1}" = "cron" ]; then
 	logAdd "[INFO] === SERVICE STOPPED ==="
 	exit 0
 elif [ "${1}" = "start" ]; then
-	RUNNING=$(ps | grep $SCRIPT_FULLFN | grep -v grep | grep /bin/sh | awk 'END { print NR }')
+	RUNNING=$(ps ww| grep $SCRIPT_FULLFN | grep -v grep | grep /bin/sh | awk 'END { print NR }')
 	if [ $RUNNING -gt 1 ]; then
 		logAdd "[INFO] === SERVICE ALREADY RUNNING ==="
 		exit 0
@@ -177,13 +177,13 @@ elif [ "${1}" = "start" ]; then
 	wait
 	exit 0
 elif [ "${1}" = "stop" ]; then
-	ps w | grep -v grep | grep "ash ${0}" | sed 's/ \+/|/g' | sed 's/^|//' | cut -d '|' -f 1 | grep -v "^$$" | while read pidhandle; do
+	ps ww | grep -v grep | grep "ash ${0}" | sed 's/ \+/|/g' | sed 's/^|//' | cut -d '|' -f 1 | grep -v "^$$" | while read pidhandle; do
 		echo "[INFO] Terminating old service instance [${pidhandle}] ..."
 		kill -9 "${pidhandle}"
 	done
 	#
 	# Check if parts of the service are still running.
-	if [ "$(ps w | grep -v grep | grep "ash ${0}" | sed 's/ \+/|/g' | sed 's/^|//' | cut -d '|' -f 1 | grep -v "^$$" | grep -c "^")" -gt 1 ]; then
+	if [ "$(ps ww | grep -v grep | grep "ash ${0}" | sed 's/ \+/|/g' | sed 's/^|//' | cut -d '|' -f 1 | grep -v "^$$" | grep -c "^")" -gt 1 ]; then
 		logAdd "[ERROR] === SERVICE FAILED TO STOP ==="
 		exit 99
 	fi

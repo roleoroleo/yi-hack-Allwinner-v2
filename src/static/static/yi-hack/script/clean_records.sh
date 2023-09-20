727c43958ffb1ab62c3d047957cb409692f54867
roleo <roleo@iperbole.bologna.it>
2023-09-20 18:47:07 +0200

    Fix wrong order

--- src/static/static/yi-hack/script/clean_records.sh
+++ src/static/static/yi-hack/script/clean_records.sh
@@ -20,14 +20,14 @@ MAX_AVI_NUMBER=10
 USED_SPACE_LIMIT=$((100-$1))
 
 cd /tmp/sd/record/timelapse
-AVI_NUMBER=`ls -lt | grep .avi | awk 'END{print NR}'`
+AVI_NUMBER=`ls -lr | grep .avi | awk 'END{print NR}'`
 while [ "$AVI_NUMBER" -gt "$MAX_AVI_NUMBER" ]
 do
-    OLD_FILE=`ls -lt | grep .avi | awk 'END{print}' | awk '{print $9}'`
+    OLD_FILE=`ls -lr | grep .avi | awk 'END{print}' | awk '{print $9}'`
     if [ ! -z "$OLD_FILE" ]; then
         echo "Deleting file $OLD_FILE"
         rm -f $OLD_FILE
-        AVI_NUMBER=`ls -lt | grep .avi | awk 'END{print NR}'`
+        AVI_NUMBER=`ls -lr | grep .avi | awk 'END{print NR}'`
     else
         break
     fi
@@ -42,7 +42,7 @@ fi
 
 while [ "$USED_SPACE" -gt "$USED_SPACE_LIMIT" ]
 do
-    OLD_DIR=`ls -lt | grep -v tmp | grep -v timelapse | awk 'END{print}' | awk '{print $9}'`
+    OLD_DIR=`ls -lr | grep -v tmp | grep -v timelapse | awk 'END{print}' | awk '{print $9}'`
     if [ ! -z "$OLD_DIR" ]; then
         echo "Deleting dir $OLD_DIR"
         rm -rf $OLD_DIR

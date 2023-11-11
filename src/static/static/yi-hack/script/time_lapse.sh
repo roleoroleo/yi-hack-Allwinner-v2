bd546f201aca85d97a8968833c1a64401880b683
roleo <roleo@iperbole.bologna.it>
2023-11-11 21:45:07 +0100

    Add option TIMELAPSE_FTP_SAME_NAME

--- src/static/static/yi-hack/script/time_lapse.sh
+++ src/static/static/yi-hack/script/time_lapse.sh
@@ -20,7 +20,7 @@ export LD_LIBRARY_PATH=/lib:/usr/lib:/home/lib:/home/qigan/lib:/home/app/localli
 FOLDER_TO_SAVE="/tmp/sd/record/timelapse"
 #
 # Runtime Variables.
-SCRIPT_NAME="ftppush_tl"
+SCRIPT_NAME="time_lapse"
 LOGFILE="/tmp/${SCRIPT_NAME}.log"
 LOG_MAX_LINES="200"
 
@@ -30,58 +30,58 @@ LOG_MAX_LINES="200"
 # -----------------------------------------------------
 lbasename ()
 {
-	echo "${1}" | sed "s/.*\///"
+    echo "${1}" | sed "s/.*\///"
 }
 
 
 
 logAdd ()
 {
-	TMP_DATETIME="$(date '+%Y-%m-%d [%H-%M-%S]')"
-	TMP_LOGSTREAM="$(tail -n ${LOG_MAX_LINES} ${LOGFILE} 2>/dev/null)"
-	echo "${TMP_LOGSTREAM}" > "$LOGFILE"
-	echo "${TMP_DATETIME} $*" >> "${LOGFILE}"
-	echo "${TMP_DATETIME} $*"
-	return 0
+    TMP_DATETIME="$(date '+%Y-%m-%d [%H-%M-%S]')"
+    TMP_LOGSTREAM="$(tail -n ${LOG_MAX_LINES} ${LOGFILE} 2>/dev/null)"
+    echo "${TMP_LOGSTREAM}" > "$LOGFILE"
+    echo "${TMP_DATETIME} $*" >> "${LOGFILE}"
+    echo "${TMP_DATETIME} $*"
+    return 0
 }
 
 
 
 uploadToFtp ()
 {
-	#
-	# Usage:			uploadToFtp -- "[FULLFN]"
-	# Example:			uploadToFtp -- "/tmp/test.txt"
-	# Purpose:
-	# 	Uploads file to FTP
-	#
-	# Returns:
-	# 	"0" on SUCCESS
-	# 	"1" on FAILURE
-	#
-	# Consts.
-	FTP_HOST="$(get_config FTP_HOST)"
-	FTP_DIR="$(get_config FTP_DIR)"
-	FTP_USERNAME="$(get_config FTP_USERNAME)"
-	FTP_PASSWORD="$(get_config FTP_PASSWORD)"
-	#
-	# Variables.
-	UTF_FULLFN="${2}"
-	if [ ! -z "${FTP_DIR}" ]; then
-		# Create directory on FTP server
-		echo -e "USER ${FTP_USERNAME}\r\nPASS ${FTP_PASSWORD}\r\nmkd ${FTP_DIR}\r\nquit\r\n" | nc -w 5 ${FTP_HOST} 21 | grep "${FTP_DIR}"
-		FTP_DIR="${FTP_DIR}/timelapse"
-		echo -e "USER ${FTP_USERNAME}\r\nPASS ${FTP_PASSWORD}\r\nmkd ${FTP_DIR}\r\nquit\r\n" | nc -w 5 ${FTP_HOST} 21 | grep "${FTP_DIR}"
-		FTP_DIR="${FTP_DIR}/"
-	fi
-	#
-	if ( ! ftpput -u "${FTP_USERNAME}" -p "${FTP_PASSWORD}" "${FTP_HOST}" "${FTP_DIR}$(lbasename "${UTF_FULLFN}")" "${UTF_FULLFN}" ); then
-		logAdd "[ERROR] uploadToFtp: ftpput FAILED."
-		return 1
-	fi
-	#
-	# Return SUCCESS.
-	return 0
+    #
+    # Usage:			uploadToFtp -- "[FULLFN]"
+    # Example:			uploadToFtp -- "/tmp/test.txt"
+    # Purpose:
+    # 	Uploads file to FTP
+    #
+    # Returns:
+    # 	"0" on SUCCESS
+    # 	"1" on FAILURE
+    #
+    # Consts:
+    FTP_HOST="$(get_config FTP_HOST)"
+    FTP_DIR="$(get_config FTP_DIR)"
+    FTP_USERNAME="$(get_config FTP_USERNAME)"
+    FTP_PASSWORD="$(get_config FTP_PASSWORD)"
+    #
+    # Variables:
+    UTF_FULLFN="${2}"
+    if [ ! -z "${FTP_DIR}" ]; then
+        # Create directory on FTP server
+        echo -e "USER ${FTP_USERNAME}\r\nPASS ${FTP_PASSWORD}\r\nmkd ${FTP_DIR}\r\nquit\r\n" | nc -w 5 ${FTP_HOST} 21 | grep "${FTP_DIR}"
+        FTP_DIR="${FTP_DIR}/timelapse"
+        echo -e "USER ${FTP_USERNAME}\r\nPASS ${FTP_PASSWORD}\r\nmkd ${FTP_DIR}\r\nquit\r\n" | nc -w 5 ${FTP_HOST} 21 | grep "${FTP_DIR}"
+        FTP_DIR="${FTP_DIR}/"
+    fi
+    #
+    if ( ! ftpput -u "${FTP_USERNAME}" -p "${FTP_PASSWORD}" "${FTP_HOST}" "${FTP_DIR}$(lbasename "${UTF_FULLFN}")" "${UTF_FULLFN}" ); then
+        logAdd "[ERROR] uploadToFtp: ftpput FAILED."
+        return 1
+    fi
+    #
+    # Return SUCCESS.
+    return 0
 }
 
 # ---------------------------------------------------
@@ -89,19 +89,19 @@ uploadToFtp ()
 # ---------------------------------------------------
 #
 
-logAdd "[INFO] === STARTING FTPPUT_TL.SH ==="
+logAdd "[INFO] === STARTING TIME_LAPSE.SH ==="
 
 if [[ $(get_config SNAPSHOT_LOW) == "no" ]] ; then
-	RES="high"
+    RES="high"
 else
-	RES="low"
+    RES="low"
 fi
 
 TZ_LOCAL=$(get_config TIMEZONE)
 if [ -z $TZ_LOCAL ]; then
-	DATE_LOCAL=$(date +%Y-%m-%d_%H-%M-00)
+    DATE_LOCAL=$(date +%Y-%m-%d_%H-%M-00)
 else
-	DATE_LOCAL=$(TZ=$TZ_LOCAL date +%Y-%m-%d_%H-%M-00)
+    DATE_LOCAL=$(TZ=$TZ_LOCAL date +%Y-%m-%d_%H-%M-00)
 fi
 
 logAdd "[INFO] starting capture."
@@ -109,20 +109,27 @@ mkdir -p $FOLDER_TO_SAVE
 imggrabber -m $MODEL_SUFFIX -r $RES -w > $FOLDER_TO_SAVE/$DATE_LOCAL.jpg
 
 if [ -s $FOLDER_TO_SAVE/$DATE_LOCAL.jpg ]; then
-	logAdd "[INFO] capture completed."
+    logAdd "[INFO] capture completed."
 
-	if [ "$1" == "yes" ] ; then
-		logAdd "[INFO] starting ftp put."
-		if ( ! uploadToFtp -- $FOLDER_TO_SAVE/$DATE_LOCAL.jpg ); then
-			logAdd "[ERROR] uploadToFtp FAILED - [$FOLDER_TO_SAVE/$DATE_LOCAL.jpg]."
-		else
-			rm -f $FOLDER_TO_SAVE/$DATE_LOCAL.jpg
-			logAdd "[INFO] ftp put completed and local file removed."
-		fi
-	fi
+    if [ "$1" == "yes" ] ; then
+        if [[ $(get_config TIMELAPSE_FTP_SAME_NAME) == "yes" ]] ; then
+            mv $FOLDER_TO_SAVE/$DATE_LOCAL.jpg $FOLDER_TO_SAVE/snapshot.jpg
+            FILE_TO_PUSH=$FOLDER_TO_SAVE/snapshot.jpg
+        else
+            FILE_TO_PUSH=$FOLDER_TO_SAVE/$DATE_LOCAL.jpg
+        fi
+
+        logAdd "[INFO] starting ftp put."
+        if ( ! uploadToFtp -- $FILE_TO_PUSH ); then
+            logAdd "[ERROR] uploadToFtp FAILED - [$FILE_TO_PUSH]."
+        else
+            rm -f $FILE_TO_PUSH
+            logAdd "[INFO] ftp put completed and local file removed."
+        fi
+    fi
 else
-	logAdd "[ERROR] capture error."
+    logAdd "[ERROR] capture error."
 fi
 
 
-logAdd "[INFO] === FTPPUT_TL.SH COMPLETED ==="
+logAdd "[INFO] === TIME_LAPSE.SH COMPLETED ==="

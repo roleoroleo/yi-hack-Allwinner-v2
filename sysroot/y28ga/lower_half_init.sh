99dca5e653fa22fa6296dcd1c02d485a93fb942f
roleo <roleo@iperbole.bologna.it>
2025-02-28 14:53:28 +0100

    Add support for y28ga v12

--- sysroot/y28ga/lower_half_init.sh
+++ sysroot/y28ga/lower_half_init.sh
@@ -19,6 +19,8 @@ else
         insmod /backup/ko/8188fu.ko
     elif [ -f /backup/ko/8189fs.ko ];then
         insmod /backup/ko/8189fs.ko
+    elif [ -f /backup/ko/atbm603x_wifi_usb.ko ];then
+        insmod /backup/ko/atbm603x_wifi_usb.ko
     elif [ -f /backup/ko/ssv6x5x.ko ];then
         if [ -f /home/base/firmware/ssv6x5x/ssv6x5x-wifi.cfg ];then
             insmod /backup/ko/ssv6x5x.ko stacfgpath="/home/base/firmware/ssv6x5x/ssv6x5x-wifi.cfg"
@@ -68,6 +70,13 @@ ethmac=d2:`ifconfig ${NETWORK_IFACE} |grep HWaddr|cut -d' ' -f10|cut -d: -f2-`
 ifconfig eth0 hw ether $ethmac
 ifconfig eth0 up
 
+HOMEVER=$(cat /home/homever)
+HV=${HOMEVER:0:2}
+
+if [ "$HV" == "11" ] || [ "$HV" == "12" ]; then
+    ln -s /home/model/BodyVehicleAnimal3.model /tmp/BodyVehicleAnimal3.model
+fi
+
 echo "============================================= home low_half_init.sh... ========================================="
 echo "============================================= begin to start app... ========================================="
 cd /home/app
@@ -79,6 +88,28 @@ fi
 if [ -f "/tmp/sd/Factory/factory_test.sh" ]; then
 	/tmp/sd/Factory/config.sh
 	exit
+fi
+
+#export LD_LIBRARY_PATH=/tmp/:$LD_LIBRARY_PATH
+#export PATH=/home/app:/home/app/script:$PATH
+export LD_LIBRARY_PATH=/home/app/locallib:/home/app/script:$LD_LIBRARY_PATH:/tmp
+echo $LD_LIBRARY_PATH
+
+if [ "$HV" == "11" ] || [ "$HV" == "12" ]; then
+    if [ -f "/tmp/sd/log_tools.tar.gz" ];then
+        echo "run log_tools start."
+        if [ ! -d /tmp/sd/log_tools ];then
+            cd /tmp/sd
+            mkdir log_tools
+        fi
+        cd /tmp/sd
+        tar -zxvf log_tools.tar.gz -C /tmp/sd/log_tools
+        chmod +x /tmp/sd/log_tools/run_log_app.sh
+        source /tmp/sd/log_tools/run_log_app.sh
+        cd -
+        echo "run log_tools end."
+        #exit
+    fi
 fi
 
 mount --bind /tmp/sd/yi-hack/script/wifidhcp.sh /home/app/script/wifidhcp.sh
@@ -98,7 +129,10 @@ LD_PRELOAD=/tmp/sd/yi-hack/lib/ipc_multiplex.so ./dispatch &
 #./watch_process &
 
 chmod 777 /tmp/sd/debug.sh
-sh /tmp/sd/debug.sh &
+if [ -f "/tmp/sd/debug.sh" ]; then
+    echo "calling /tmp/sd/debug.sh"
+    sh /tmp/sd/debug.sh &
+fi
 
 chmod 755 /tmp/sd/yi-hack/script/system.sh
 sh /tmp/sd/yi-hack/script/system.sh &

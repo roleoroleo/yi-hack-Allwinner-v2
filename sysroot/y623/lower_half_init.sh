e3311a56f7939ac131ee5234f9acec7ff9f61ce7
Iago Veiga <iago@veiga.top>
2026-01-31 22:32:54 +0000

    feat: Add watermark disable feature for confirmed models (y623, r37gb, y28ga)
    Implements the ability to disable camera watermark via web UI for models
    with confirmed watermark presence.
    
    Features:
    - Web UI checkbox in configuration page
    - Bind-mount mechanism to replace watermark BMP files with blank images
    - Optimized 16x16px blank BMP files (822 bytes each)
    - CUSTOM_WATERMARK configuration option (default: disabled)
    - Boot script modifications for 3 confirmed models only
    
    Models with confirmed watermark:
    - y623 (Yi Pro 2K Home) - validated by @QaAndreii in #1069
    - r37gb (Yi Dome Guard) - validated in hardware testing
    - y28ga (Kami mini home) - uses main_kami.bmp watermark files
    
    Technical implementation:
    - Checks CUSTOM_WATERMARK=yes in system.conf
    - Verifies existence of blank BMP files
    - Creates bind-mounts before camera dispatch process
    - Supports both standard (main/sub.bmp) and Kami variants
    
    Scalability:
    - Only confirmed models modified to avoid overhead
    - Architecture allows easy expansion if other users report watermark
    - We don't know all models with watermark, designed for future extension
    
    Closes #1069
    Related to #19

--- sysroot/y623/lower_half_init.sh
+++ sysroot/y623/lower_half_init.sh
@@ -193,6 +193,42 @@ else
     mount --bind /tmp/sd/yi-hack/script/ethdhcp.sh /home/app/script/ethdhcp.sh
     mount --bind /tmp/sd/yi-hack/script/ethdhcp.sh /backup/tools/ethdhcp.sh
 
+    # Custom watermark: override with blank BMPs if enabled
+YI_HACK_PREFIX="/tmp/sd/yi-hack"
+CONF_FILE="etc/system.conf"
+get_config() {
+    key=$1
+    grep -w $1 $YI_HACK_PREFIX/$CONF_FILE | cut -d "=" -f2
+}
+if [ "$(get_config CUSTOM_WATERMARK)" == "yes" ]; then
+        # Main watermark
+        if [ -f /tmp/sd/yi-hack/etc/watermark/main_blank.bmp ] && [ -f /home/app/main.bmp ]; then
+            mount --bind /tmp/sd/yi-hack/etc/watermark/main_blank.bmp /home/app/main.bmp
+        elif [ -f /tmp/sd/yi-hack/etc/watermark/main_kami_blank.bmp ] && [ -f /home/app/main_kami.bmp ]; then
+            mount --bind /tmp/sd/yi-hack/etc/watermark/main_kami_blank.bmp /home/app/main_kami.bmp
+        fi
+
+        # Sub watermark
+        if [ -f /tmp/sd/yi-hack/etc/watermark/sub_blank.bmp ] && [ -f /home/app/sub.bmp ]; then
+            mount --bind /tmp/sd/yi-hack/etc/watermark/sub_blank.bmp /home/app/sub.bmp
+        elif [ -f /tmp/sd/yi-hack/etc/watermark/sub_kami_blank.bmp ] && [ -f /home/app/sub_kami.bmp ]; then
+            mount --bind /tmp/sd/yi-hack/etc/watermark/sub_kami_blank.bmp /home/app/sub_kami.bmp
+        fi
+    # User-provided custom watermark (legacy support)
+    elif [ -f /tmp/sd/yi-hack/watermark/main.bmp ] && [ -f /home/app/main.bmp ]; then
+        mount --bind /tmp/sd/yi-hack/watermark/main.bmp /home/app/main.bmp
+    elif [ -f /tmp/sd/yi-hack/watermark/main_kami.bmp ] && [ -f /home/app/main_kami.bmp ]; then
+        mount --bind /tmp/sd/yi-hack/watermark/main_kami.bmp /home/app/main_kami.bmp
+    fi
+
+if [ "$(get_config CUSTOM_WATERMARK)" != "yes" ]; then
+        if [ -f /tmp/sd/yi-hack/watermark/sub.bmp ] && [ -f /home/app/sub.bmp ]; then
+            mount --bind /tmp/sd/yi-hack/watermark/sub.bmp /home/app/sub.bmp
+        elif [ -f /tmp/sd/yi-hack/watermark/sub_kami.bmp ] && [ -f /home/app/sub_kami.bmp ]; then
+            mount --bind /tmp/sd/yi-hack/watermark/sub_kami.bmp /home/app/sub_kami.bmp
+        fi
+    fi
+
     LD_PRELOAD=/tmp/sd/yi-hack/lib/ipc_multiplex.so ./dispatch &
 #    sleep 2
 #    ./rmm &
